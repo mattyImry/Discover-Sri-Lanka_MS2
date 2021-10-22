df01303bc12836293ed19591d5da19c6484a6c78
mattyImry <matteo.imeri@gmail.com>
2021-10-22 10:05:52 +0000

    Add animation to Anura page

---
+++ assets/js/animation.js
@@ -0,0 +1,8 @@
+
+
+// $(document).ready(function() {
+// document.getElementById('title').classList.remove('title-out').fadeIn(1000);
+// document.getElementById('txt').classList.remove('text-out').fadeIn(1000);
+// document.getElementById('img').classList.remove('img-out').fadeIn(1000);
+
+// })
